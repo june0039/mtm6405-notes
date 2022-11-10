a55a41a99cc1b396d893082ac230b39deb49b729
Julie <june0039@algonquinlive.com>
2022-11-10 12:59:24 -0500

    end-part2

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
 @foreach ($notes as $note)
 <div class="col-12 col-md-6 col-lg-4">
-<a href="/notes/{{ $note['id']" class="text-decoration-none text-body">
+<a href="/notes/{{ $note['id']}}" class="text-decoration-none text-body">
 <div class="card mb-3">
   <h5 class="card-header">{{ $note['title']}}</h5>
    <div classs="card-body">
