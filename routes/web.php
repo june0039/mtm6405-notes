3a710efc344234eb6e9f408df6f4e5fbf0c114b3
Julie <june0039@algonquinlive.com>
2022-11-10 12:10:31 -0500

    p

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Note;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ |--------------------------------------------------------------------------
+ | Web Routes
+ |--------------------------------------------------------------------------
+ |
+ | Here is where you can register web routes for your application. These
+ | routes are loaded by the RouteServiceProvider within a group which
+ | contains the "web" middleware group. Now create something great!
+ |
+ */
 
-Route::get('/', function () {
-    $title = "Notes";
-    return view('notes', ['title' => $title]);
-});
+Route::get(
+    '/', function () {
+        $title = "Notes";
+        $notes = Note::all();
 
-Route::get('/notes/{note}', function ($id) {
-    $title = "Note {$id}";
-    $text = "This is note {$id}";
-    return view('note', ['title' => $title, 'text' => $text]);
-});
+        return view('notes', ['title' => $title, 'notes' => $notes]);
+    }
+);
+
+Route::get(
+    '/notes/{note}', function ($id) {
+        $title = "Note {$id}";
+        $text = "This is note {$id}";
+        return view('note', ['title' => $title, 'text' => $text]);
+    }
+);
