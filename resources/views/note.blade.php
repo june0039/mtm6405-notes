4b1112c7c0c7771dfe2ca3fe9ef1a7fab7cb33f4
Julie <june0039@algonquinlive.com>
2022-11-24 12:40:25 -0500

    edit

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -1,6 +1,15 @@
 <x-layout>
   <x-slot name="title">{{ $title }}</x-slot>
+  <div class="row d-flex align-items-center">
+  <div class="col-9">
+
   <h1 class="display-4 my-5">{{ $title }}</h1>
+  </div>
+  <div class="col-3">
+   <a href="/notes/{{$id}}/edit" class="text-decoration-none text-body">Edit</a>
+   </div>
+   </div>
+
   <p class="fs-4">{{ $text }}</p>
   <a href="/" class="text-decoration-none">&lt; Back</a>
 </x-layout>
